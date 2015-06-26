4ce8313e3b2d034fe263efebba2d731365b9548a
Henrik Blidh <henrik.blidh@nedomkull.com>
2015-06-27 00:54:30 +0200

    First templates under way.

--- restpyberryimu/views.py
+++ restpyberryimu/views.py
@@ -23,6 +23,9 @@ import datetime
 import numpy as np
 
 from flask import render_template, jsonify, request, redirect, make_response
+from flask_wtf import Form
+from wtforms import StringField, SelectField
+from wtforms.validators import DataRequired
 from restpyberryimu import app
 
 @app.route('/', methods=['GET', ])
@@ -30,14 +33,19 @@ def index():
     return render_template('index.html',
                            title='Home')
 
+class MyForm(Form):
+    name = StringField('Recording Name', validators=[DataRequired()])
+    data_rate = SelectField('Data Rate', choices=((50, '50'), (100, '100'), (150, '150'), (200, '200')), default=200)
+
 @app.route('/new_session', methods=['GET', 'POST'])
 def new_session():
-    return render_template('index.html',
-                           title='New Session Page')
+    return render_template('new_session.html',
+                           title='New Session Page',
+                           form=MyForm())
 
 @app.route('/past_sessions', methods=['GET'])
 def past_sessions():
-    return render_template('index.html',
+    return render_template('past_sessions.html',
                            title='Past Sessions Page')
 
 
